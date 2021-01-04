5c2b8e0164efeaf12b3b8c4c626caf8bafdabeb3
Tiggum <caden.james.reynolds@gmail.com>
2021-01-04 21:21:42 +0100

    initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Im workin yo!</div>;
+  }
+}
+
+export default Navbar;
